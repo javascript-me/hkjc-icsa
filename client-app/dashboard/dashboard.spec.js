2fe3126f9559978c4ced2708ebb91d9a60a900f5
gabor_czigola <gabor_czigola@epam.com>
2016-11-07 16:31:19 +0800

    fix unit test

--- client-app/dashboard/dashboard.spec.js
+++ client-app/dashboard/dashboard.spec.js
@@ -6,6 +6,6 @@ import Dashboard from './dashboard'
 describe('<Dashboard />', () => {
 	it('renders a dashboard div', () => {
 		const dashboard = shallow(<Dashboard />)
-		expect(dashboard.find('div.dashboard')).to.have.length(1)
+		expect(dashboard.find('div.row-dashboard')).to.have.length(1)
 	})
 })
--- client-app/navigation/navigation.spec.js
+++ client-app/navigation/navigation.spec.js
@@ -6,6 +6,6 @@ import Navigation from './navigation'
 describe('<Navigation />', () => {
 	it('renders a navigation div', () => {
 		const navigation = shallow(<Navigation />)
-		expect(navigation.find('div.navigation')).to.have.length(1)
+		expect(navigation.find('div.row-navigation')).to.have.length(1)
 	})
 })
