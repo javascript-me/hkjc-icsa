c7c765acf0f7adb82be7f0a0ce12be55f2ae4142
Tanzeem Bhatti <Tanzeem_Bhatti@epam.com>
2016-11-10 09:54:45 +0800

    Added Structure for table auditlog

--- client-app/auditlogContainer/auditlogContainer.js
+++ client-app/auditlogContainer/auditlogContainer.js
@@ -6,18 +6,163 @@ export default React.createClass({
     displayName: 'Audit',
     render() {
         return (
-            <div className='container'>
+            <div className='auditlog container'>
                 <div className='row'>
                     <div className="col-md-6">
-                        <Calendar />
+                       <Calendar />
                     </div>
                     <div className="col-md-6">
 
                     </div>
                 </div>
                 <div className='row'>
-                    <div className="col-md-6">
-
+                    <div className="col-xs-12">
+                        <table className="table table-striped auditlog-table">
+                          <thead className="table-header">
+                            <tr>
+                              <th>Date/Time</th>
+                              <th>User ID</th>
+                              <th>User Name</th>
+                              <th>Type</th>
+                               <th>Function/Module</th>
+                              <th>Function Event Detail</th>
+                              <th>User Role</th>
+                              <th>IP Address</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>@fat</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                            <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                             <tr>
+                             <td>23 September 2016 19:12:01</td>
+                              <td>Larry</td>
+                              <td>the Bird</td>
+                              <td>@twitter</td>
+                              <td>23 September 2016 19:12:01</td>
+                              <td>Mark</td>
+                              <td>Otto</td>
+                              <td>@mdo</td>
+                            </tr>
+                          </tbody>
+                        </table>
                     </div>
                     <div className="col-md-6">
 
