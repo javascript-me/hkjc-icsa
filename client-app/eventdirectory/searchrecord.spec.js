cf301bff68bc26e7aee2a17ef878aac7c3b53e1c
Bing Hu <Bing_Hu@epam.com>
2016-11-16 18:29:04 +0800

    unit test

--- client-app/eventdirectory/searchrecord.spec.js
+++ client-app/eventdirectory/searchrecord.spec.js
@@ -5,7 +5,13 @@ import SearchRecord from './searchrecord'
 
 describe('<SearchRecord />', () => {
 	it('renders a SearchRecord div', () => {
-		const searchRecord = shallow(<SearchRecord />)
+		const record = {
+			"active": 1,
+			"t1": "ARS",
+			"t2": "ASV"
+		}
+
+		const searchRecord = shallow(<SearchRecord record={record} />)
 		expect(searchRecord.find('div.ed-record')).to.have.length(1)
 	})
 })
