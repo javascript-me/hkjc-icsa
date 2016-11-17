8a15768e7832cfb8cd99924a312d38332815a93b
Bing Hu <Bing_Hu@epam.com>
2016-11-17 18:58:51 +0800

    mock data

--- client-app/eventdirectory/searchrecord.js
+++ client-app/eventdirectory/searchrecord.js
@@ -18,7 +18,7 @@ export default React.createClass({
 			<div ref='root' className='ed-record'>
 				<span className={statusClasses} title={this.props.record.status} />
 				<span className='ed-record-text'>
-					<span className={this.props.record.active === 1 ? 'active' : ''} title={this.props.record.t1}>{this.props.record.t1}</span> vs <span className={this.props.record.active === 2 ? 'active' : ''} title={this.props.record.t2}>{this.props.record.t2}</span>
+					<span className={this.props.record.active === 1 ? 'active' : ''} title={this.props.record.t1Tip}>{this.props.record.t1}</span> vs <span className={this.props.record.active === 2 ? 'active' : ''} title={this.props.record.t2Tip}>{this.props.record.t2}</span>
 				</span>
 				<span className='ed-record-icon func-e' title='Function E' />
 				<span className='ed-record-icon func-cc' title='Function CC' />
