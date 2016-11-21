abfc7ef2085bb0ffcc2f856f5f41777832af9870
Sherry Hua <Sherry_Hua@epam.com>
2016-11-21 12:02:37 +0800

    fix bug

--- client-app/systembar/systembar.spec.js
+++ client-app/systembar/systembar.spec.js
@@ -15,10 +15,10 @@ describe('<Systembar />', () => {
 	})
 
 	it('set props', () => {
-	    const wrapper = mount(<Systembar bar="baz" />);
-	    expect(wrapper.props().bar).to.equal('baz');
-	    wrapper.setProps({ bar: 'foo' });
-	    expect(wrapper.props().bar).to.equal('foo');
+	    const wrapper = mount(<Systembar bar='baz' />)
+	    expect(wrapper.props().bar).to.equal('baz')
+	    wrapper.setProps({ bar: 'foo' })
+	    expect(wrapper.props().bar).to.equal('foo')
 	})
 
 	it('click event', () => {
@@ -31,18 +31,18 @@ describe('<Systembar />', () => {
 	    const wrapper = mount(<Systembar />)
 	    expect(Systembar.prototype.componentDidMount).to.have.property('callCount', 1)
 	    Systembar.prototype.componentDidMount.restore()
-	  });
+	  })
 
 	it('Show popup, when click the time showing area', () => {
 		let systembar = shallow(<Systembar />)
 		let childLength = systembar.find('div.row-systembar').children().length
 		systembar.find('a').simulate('click')
-		expect(systembar.find('div.row-systembar').children().length).to.equal(childLength+1)
+		expect(systembar.find('div.row-systembar').children().length).to.equal(childLength + 1)
 	})
 
 	it('send the request to get time', () => {
 		const getTime = sinon.stub().returns({})
-		rewire(Systembar.__set__('SystembarService', {getClock:getTime}))
+		rewire(Systembar.__set__('SystembarService', {getClock: getTime}))
 
 		const getClock = Systembar.__get__('getClock')
 
