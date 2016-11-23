131aacb7384fd0b789b85c8ed2303976e4675bba
Tanzeem Bhatti <tanzeem_bhatti@epam.com>
2016-11-23 17:36:00 +0800

    Resolved test failure issues

--- client-app/tabulardata/tabulardata.js
+++ client-app/tabulardata/tabulardata.js
@@ -1,155 +1,156 @@
 import React from 'react'
 
 export default React.createClass({
-    propTypes: {
-        onChange: React.PropTypes.func
-    },
+	propTypes: {
+		onChange: React.PropTypes.func,
+		displayCheckBox: React.PropTypes.bool,
+		headers: React.PropTypes.array,
+		dataCollection: React.PropTypes.array,
+		onClickSorting: React.PropTypes.func
+	},
 
-    getInitialState () {
-        /*Checks whether check box needs to be add in headers array*/
-        // TODO: This code should be moved somewhere.
-        if (this.props.displayCheckBox) {
-            this.props.headers.splice(0, 0, {'id': 0, label: '', fieldName: '', sortingClass: '', addCheckBox: true})
-        }
-        return {}
-    },
+	getInitialState () {
+		/* Checks whether check box needs to be add in headers array */
+		// TODO: This code should be moved somewhere.
+		if (this.props.displayCheckBox) {
+			this.props.headers.splice(0, 0, {'id': 0, label: '', fieldName: '', sortingClass: '', addCheckBox: true})
+		}
+		return {}
+	},
 
-    setToNoArrow (headers) {
-        for (var i = 0; i < headers.length; i++) {
-            headers[i].sortingClass = 'no-arrow'
-        }
+	setToNoArrow (headers) {
+		for (var i = 0; i < headers.length; i++) {
+			headers[i].sortingClass = 'no-arrow'
+		}
 
-        return headers
-    },
+		return headers
+	},
 
-    updateColumnSortingArrow (headers, fieldName) {
-        var element = this.findHeader(headers, fieldName)
+	updateColumnSortingArrow (headers, fieldName) {
+		var element = this.findHeader(headers, fieldName)
 
-        var oldSortingClass = element.sortingClass
+		var oldSortingClass = element.sortingClass
 
-        headers = this.setToNoArrow(headers)
+		headers = this.setToNoArrow(headers)
 
-        element.sortingClass = this.transformSortingClass(oldSortingClass)
+		element.sortingClass = this.transformSortingClass(oldSortingClass)
 
-        return headers
-    },
+		return headers
+	},
 
-    transformSortingClass (value) {
-        if (value === 'no-arrow') return 'down-arrow'
-        if (value === 'down-arrow') return 'up-arrow'
-        if (value === 'up-arrow') return 'down-arrow'
-        return ''
-    },
+	transformSortingClass (value) {
+		if (value === 'no-arrow') return 'down-arrow'
+		if (value === 'down-arrow') return 'up-arrow'
+		if (value === 'up-arrow') return 'down-arrow'
+		return ''
+	},
 
-    findHeader (headers, fieldName) {
-        for (var i = 0; i < headers.length; i++) {
-            var element = headers[i]
+	findHeader (headers, fieldName) {
+		for (var i = 0; i < headers.length; i++) {
+			var element = headers[i]
 
-            if (element.fieldName === fieldName) {
-                return element
-            }
-        }
+			if (element.fieldName === fieldName) {
+				return element
+			}
+		}
 
-        return null
-    },
+		return null
+	},
 
-    parseToOrder (value) {
-        // TODO: these names should be extract somewhere.
+	parseToOrder (value) {
+		// TODO: these names should be extract somewhere.
 
-        if (value === 'no-arrow') return 'NO_ORDER'
-        if (value === 'up-arrow') return 'ASCEND'
-        if (value === 'down-arrow') return 'DESCEND'
-        return ''
-    },
+		if (value === 'no-arrow') return 'NO_ORDER'
+		if (value === 'up-arrow') return 'ASCEND'
+		if (value === 'down-arrow') return 'DESCEND'
+		return ''
+	},
 
-    onItemClick (event) {
-        var fieldName = event.target.id
+	onItemClick (event) {
+		var fieldName = event.target.id
 
-        this.setState({
-            headers: this.updateColumnSortingArrow(this.props.headers, fieldName)
-        })
+		this.setState({
+			headers: this.updateColumnSortingArrow(this.props.headers, fieldName)
+		})
 
-        var header = this.findHeader(this.props.headers, fieldName)
+		var header = this.findHeader(this.props.headers, fieldName)
 
-        var order = this.parseToOrder(header.sortingClass)
+		var order = this.parseToOrder(header.sortingClass)
 
-        var sortingObject = {fieldName: fieldName, order: order}
+		var sortingObject = {fieldName: fieldName, order: order}
 
-        this.props.onClickSorting(1, sortingObject, null)
-    },
+		this.props.onClickSorting(1, sortingObject, null)
+	},
 
-    formatColumnVal (columnVal) {
-        return columnVal || 'N / A'
-    },
+	formatColumnVal (columnVal) {
+		return columnVal || 'N / A'
+	},
 
-    // TODO: below long HTML should be extracted to a method.
-    // TODO: fieldName like date_time is appeared in 2 places. Need to combine.
+	// TODO: below long HTML should be extracted to a method.
+	// TODO: fieldName like date_time is appeared in 2 places. Need to combine.
 
-    renderRows () {
-        if (this.props.dataCollection.length === 0) {
-            let rowArr = [<div className='nodata'>NO Results</div>]
-            let tdDom = []
-            for (let j = 0; j < 18; j++) {
-                tdDom.push(<td />)
-            }
-            for (let i = 0; i < 10; i++) {
-                rowArr.push(<tr className='nodata-tr'>{tdDom}</tr>)
-            }
-            return rowArr
-        }
-        return this.props.dataCollection.map((row) => {
-            return <tr>
-                {this.props.displayCheckBox ?
-                    <td><input type="checkbox" className="checkbox" name="user-profile"/></td> : null}
-                {this.props.headers.map(
-                    (header, i) => {
-                        if (header.fieldName != "") {
-                            return <td>{this.formatColumnVal(row[header.fieldName])}</td>
-                        }
-                    }
-                )}
-            </tr>
-        })
-    },
-    checkAll(event) {
-        var checkboxes = document.getElementsByTagName('input')
-        if (event.target.checked) {
-            for (var i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].type == 'checkbox') {
-                    checkboxes[i].checked = true;
-                }
-            }
-        } else {
-            for (var i = 0; i < checkboxes.length; i++) {
-                if (checkboxes[i].type == 'checkbox') {
-                    checkboxes[i].checked = false;
-                }
-            }
-        }
-    },
+	renderRows () {
+		if (this.props.dataCollection.length === 0) {
+			let rowArr = [<div className='nodata'>NO Results</div>]
+			let tdDom = []
+			for (let j = 0; j < 18; j++) {
+				tdDom.push(<td />)
+			}
+			for (let i = 0; i < 10; i++) {
+				rowArr.push(<tr className='nodata-tr'>{tdDom}</tr>)
+			}
+			return rowArr
+		}
+		return this.props.dataCollection.map((row) => {
+			return <tr>
+				{this.props.displayCheckBox ? <td><input type='checkbox' className='checkbox' name='user-profile' /></td> : null}
+				{this.props.headers.map(
+					(header, i) => {
+						if (header.fieldName !== '') {
+							return <td>{this.formatColumnVal(row[header.fieldName])}</td>
+						}
+					}
+				)}
+			</tr>
+		})
+	},
+	checkAll (event) {
+		var checkboxes = document.getElementsByTagName('input')
+		if (event.target.checked) {
+			var i = 0
+			for (i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].type === 'checkbox') {
+					checkboxes[i].checked = true
+				}
+			}
+		} else {
+			for (i = 0; i < checkboxes.length; i++) {
+				if (checkboxes[i].type === 'checkbox') {
+					checkboxes[i].checked = false
+				}
+			}
+		}
+	},
 
-    render () {
-        return <table className='table-striped table auditlog-table'>
-            <thead className='table-header'>
-            <tr>
-                {
-                    this.props.headers.map(
-                        (header, i) => {
-                            return <th>
-                                { this.props.displayCheckBox && header.addCheckBox ?
-                                    <input type="checkbox" className="checkbox pull-right" id="checkall"
-                                           onChange={(e) => this.checkAll(e)}/> : null }
-                                <span id={header.fieldName} className={header.sortingClass}
-                                      onClick={this.onItemClick}>{header.label}</span>
-                            </th>
-                        }
-                    )
-                }
-            </tr>
-            </thead>
-            <tbody>
-            {this.renderRows()}
-            </tbody>
-        </table>
-    }
+	render () {
+		return <table className='table-striped table auditlog-table'>
+			<thead className='table-header'>
+				<tr>
+					{
+					this.props.headers.map(
+						(header) => {
+							return <th>
+								{ this.props.displayCheckBox && header.addCheckBox ? <input type='checkbox' className='checkbox pull-right' id='checkall' onChange={(e) => this.checkAll(e)} /> : null }
+								<span id={header.fieldName} className={header.sortingClass} onClick={this.onItemClick}>{header.label}</span>
+							</th>
+						}
+					)
+					}
+				</tr>
+			</thead>
+			<tbody>
+				{this.renderRows()}
+			</tbody>
+		</table>
+	}
 })
--- client-app/tabulardata/tabulardata.spec.js
+++ client-app/tabulardata/tabulardata.spec.js
@@ -3,35 +3,56 @@ import { shallow } from 'enzyme'
 import { assert } from 'chai'
 import Tabulardata from './tabulardata'
 
+var headers = [
+	{label: 'Date/Time', fieldName: 'date_time', sortingClass: 'no-arrow'},
+	{label: 'User ID', fieldName: 'user_id', sortingClass: 'no-arrow'},
+	{label: 'User Name', fieldName: 'user_name', sortingClass: 'no-arrow'},
+	{label: 'Type', fieldName: 'Type', sortingClass: 'no-arrow'},
+	{label: 'Function/Module', fieldName: 'function_module', sortingClass: 'no-arrow'},
+	{label: 'Function Event Detail', fieldName: 'function_event_detail', sortingClass: 'no-arrow'},
+	{label: 'User Role', fieldName: 'user_role', sortingClass: 'no-arrow'},
+	{label: 'IP Address', fieldName: 'ip_address', sortingClass: 'no-arrow'},
+	{label: 'Back End ID', fieldName: 'backend_id', sortingClass: 'no-arrow'},
+	{label: 'Front End ID', fieldName: 'frontend_id', sortingClass: 'no-arrow'},
+	{label: 'Home', fieldName: 'home', sortingClass: 'no-arrow'},
+	{label: 'Away', fieldName: 'away', sortingClass: 'no-arrow'},
+	{label: 'K.O. Time/ Game Start Time', fieldName: 'ko_time_game_start_game', sortingClass: 'no-arrow'},
+	{label: 'Bet Type', fieldName: 'bet_type', sortingClass: 'no-arrow'},
+	{label: 'Event Name', fieldName: 'event_name', sortingClass: 'no-arrow'},
+	{label: 'Error Code', fieldName: 'error_code', sortingClass: 'no-arrow'},
+	{label: 'Error Message Content', fieldName: 'error_message_content', sortingClass: 'no-arrow'},
+	{label: 'Device', fieldName: 'device', sortingClass: 'no-arrow'}
+]
+
+var dataCollection = [{
+	'date_time': '23 October 2016 10:30:30',
+	'user_id': 'Jerry.Li',
+	'user_name': 'Jerry Li',
+	'Type': 'Result',
+	'function_module': 'Master Risk Limit Log',
+	'function_event_detail': 'Update Result',
+	'user_role': 'System Administrator',
+	'ip_address': '200.3.45.33',
+	'backend_id': 'FB0125',
+	'frontend_id': 'FB0012',
+	'home': 'Hong Kong',
+	'away': 'China',
+	'ko_time_game_start_game': '16/09/2016 10:00',
+	'bet_type': 'Bet Type',
+	'event_name': 'EPC',
+	'error_code': '018',
+	'error_message_content': 'No left fields',
+	'device': 'PC'
+}]
+
 describe('<Tabulardata />', () => {
 	it('renders a table div', () => {
-		const tabulardata = shallow(<Tabulardata />)
+		const tabulardata = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />)
 		expect(tabulardata.find('table')).to.have.length(1)
 	})
 
 	it('updateColumnSortingArrow() should update column sorting arrow by fieldName', () => {
-		const instance = shallow(<Tabulardata />).instance()
-
-		var headers = [
-            {label: 'Date/Time', fieldName: 'date_time', sortingClass: 'no-arrow'},
-            {label: 'User ID', fieldName: 'user_id', sortingClass: 'no-arrow'},
-            {label: 'User Name', fieldName: 'user_name', sortingClass: 'no-arrow'},
-            {label: 'Type', fieldName: 'Type', sortingClass: 'no-arrow'},
-            {label: 'Function/Module', fieldName: 'function_module', sortingClass: 'no-arrow'},
-            {label: 'Function Event Detail', fieldName: 'function_event_detail', sortingClass: 'no-arrow'},
-            {label: 'User Role', fieldName: 'user_role', sortingClass: 'no-arrow'},
-            {label: 'IP Address', fieldName: 'ip_address', sortingClass: 'no-arrow'},
-            {label: 'Back End ID', fieldName: 'backend_id', sortingClass: 'no-arrow'},
-            {label: 'Front End ID', fieldName: 'frontend_id', sortingClass: 'no-arrow'},
-            {label: 'Home', fieldName: 'home', sortingClass: 'no-arrow'},
-            {label: 'Away', fieldName: 'away', sortingClass: 'no-arrow'},
-            {label: 'K.O. Time/ Game Start Time', fieldName: 'ko_time_game_start_game', sortingClass: 'no-arrow'},
-            {label: 'Bet Type', fieldName: 'bet_type', sortingClass: 'no-arrow'},
-            {label: 'Event Name', fieldName: 'event_name', sortingClass: 'no-arrow'},
-            {label: 'Error Code', fieldName: 'error_code', sortingClass: 'no-arrow'},
-            {label: 'Error Message Content', fieldName: 'error_message_content', sortingClass: 'no-arrow'},
-            {label: 'Device', fieldName: 'device', sortingClass: 'no-arrow'}
-		]
+		const instance = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />).instance()
 
 		var resultA = instance.updateColumnSortingArrow(headers, 'date_time')
 		assert.equal('down-arrow', resultA[0].sortingClass)
@@ -46,28 +67,7 @@ describe('<Tabulardata />', () => {
 	})
 
 	it('findHeader() should return header based on fieldName', () => {
-		const instance = shallow(<Tabulardata />).instance()
-
-		var headers = [
-            {label: 'Date/Time', fieldName: 'date_time', sortingClass: 'no-arrow'},
-            {label: 'User ID', fieldName: 'user_id', sortingClass: 'no-arrow'},
-            {label: 'User Name', fieldName: 'user_name', sortingClass: 'no-arrow'},
-            {label: 'Type', fieldName: 'Type', sortingClass: 'no-arrow'},
-            {label: 'Function/Module', fieldName: 'function_module', sortingClass: 'no-arrow'},
-            {label: 'Function Event Detail', fieldName: 'function_event_detail', sortingClass: 'no-arrow'},
-            {label: 'User Role', fieldName: 'user_role', sortingClass: 'no-arrow'},
-            {label: 'IP Address', fieldName: 'ip_address', sortingClass: 'no-arrow'},
-            {label: 'Back End ID', fieldName: 'backend_id', sortingClass: 'no-arrow'},
-            {label: 'Front End ID', fieldName: 'frontend_id', sortingClass: 'no-arrow'},
-            {label: 'Home', fieldName: 'home', sortingClass: 'no-arrow'},
-            {label: 'Away', fieldName: 'away', sortingClass: 'no-arrow'},
-            {label: 'K.O. Time/ Game Start Time', fieldName: 'ko_time_game_start_game', sortingClass: 'no-arrow'},
-            {label: 'Bet Type', fieldName: 'bet_type', sortingClass: 'no-arrow'},
-            {label: 'Event Name', fieldName: 'event_name', sortingClass: 'no-arrow'},
-            {label: 'Error Code', fieldName: 'error_code', sortingClass: 'no-arrow'},
-            {label: 'Error Message Content', fieldName: 'error_message_content', sortingClass: 'no-arrow'},
-            {label: 'Device', fieldName: 'device', sortingClass: 'no-arrow'}
-		]
+		const instance = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />).instance()
 
 		var result = instance.findHeader(headers, 'error_code')
 
@@ -75,7 +75,7 @@ describe('<Tabulardata />', () => {
 	})
 
 	it('transformSortingClass() should change sortingClass in a special way', () => {
-		const instance = shallow(<Tabulardata />).instance()
+		const instance = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />).instance()
 
 		assert.equal('down-arrow', instance.transformSortingClass('no-arrow'))
 		assert.equal('up-arrow', instance.transformSortingClass('down-arrow'))
@@ -83,28 +83,7 @@ describe('<Tabulardata />', () => {
 	})
 
 	it('setToNoArrow() will set all element.sortingClass to no-arrow', () => {
-		const instance = shallow(<Tabulardata />).instance()
-
-		var headers = [
-            {label: 'Date/Time', fieldName: 'date_time', sortingClass: 'up-arrow'},
-            {label: 'User ID', fieldName: 'user_id', sortingClass: 'down-arrow'},
-            {label: 'User Name', fieldName: 'user_name', sortingClass: 'no-arrow'},
-            {label: 'Type', fieldName: 'Type', sortingClass: 'no-arrow'},
-            {label: 'Function/Module', fieldName: 'function_module', sortingClass: 'no-arrow'},
-            {label: 'Function Event Detail', fieldName: 'function_event_detail', sortingClass: 'no-arrow'},
-            {label: 'User Role', fieldName: 'user_role', sortingClass: 'no-arrow'},
-            {label: 'IP Address', fieldName: 'ip_address', sortingClass: 'no-arrow'},
-            {label: 'Back End ID', fieldName: 'backend_id', sortingClass: 'no-arrow'},
-            {label: 'Front End ID', fieldName: 'frontend_id', sortingClass: 'no-arrow'},
-            {label: 'Home', fieldName: 'home', sortingClass: 'no-arrow'},
-            {label: 'Away', fieldName: 'away', sortingClass: 'no-arrow'},
-            {label: 'K.O. Time/ Game Start Time', fieldName: 'ko_time_game_start_game', sortingClass: 'no-arrow'},
-            {label: 'Bet Type', fieldName: 'bet_type', sortingClass: 'no-arrow'},
-            {label: 'Event Name', fieldName: 'event_name', sortingClass: 'no-arrow'},
-            {label: 'Error Code', fieldName: 'error_code', sortingClass: 'no-arrow'},
-            {label: 'Error Message Content', fieldName: 'error_message_content', sortingClass: 'no-arrow'},
-            {label: 'Device', fieldName: 'device', sortingClass: 'no-arrow'}
-		]
+		const instance = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />).instance()
 
 		var result = instance.setToNoArrow(headers)
 
@@ -113,7 +92,7 @@ describe('<Tabulardata />', () => {
 	})
 
 	it('parseToOrder() should return ASCEND, DESCEND or NO_ORDER', () => {
-		const instance = shallow(<Tabulardata />).instance()
+		const instance = shallow(<Tabulardata headers={headers} dataCollection={dataCollection} />).instance()
 
 		assert.equal('NO_ORDER', instance.parseToOrder('no-arrow'))
 		assert.equal('ASCEND', instance.parseToOrder('up-arrow'))
