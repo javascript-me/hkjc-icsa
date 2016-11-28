566ec1fed5254ea4d67af7a3d3d6f4fda1aabdbc
Steven_Weng <Steven_Weng@epam.com>
2016-11-28 16:32:58 +0800

    finish apidoc and add mock data for notice board mock api

--- server-simulator/API/notice-board/index.js
+++ server-simulator/API/notice-board/index.js
@@ -5,55 +5,71 @@ const router = express.Router()
 const jsonAlerts = require('../json/notice-alerts.json') || {}
 const jsonCriticalInformations = require('../json/notice-critical-informations.json') || {}
 
-getNoticeDataByUserName(allAlerts, allCriticalInformations, userName) {
+const getNoticeDataByUserName = (allAlerts, allCriticalInformations, userName) => {
 	let alerts = allAlerts[userName] || []
 	let criticalInformations = allCriticalInformations[userName] || []
 
 	return [].concat(alerts, criticalInformations)
 }
 
-filterOutNoticeEalierThanSixMonthsAgo(notices) {
+const filterOutNoticeEalierThanSixMonthsAgo = (notices) => {
+	let now = moment(new Date())
+	let sixMonthsBefore = now.subtract(6, 'months')
+
 	return notices.filter((notice) => {
 		return moment(notice.system_distribution_time).isAfter(sixMonthsBefore)
 	})
 }
 
-sortNoticesBySystemDistributionTimeDESC(a, b) {
+const sortNoticesBySystemDistributionTimeDESC = (a, b) => {
 	return moment(b.system_distribution_time).valueOf() - moment(a.system_distribution_time).valueOf()
 }
 
+
+/**
+ * @api {GET} /notice-board/ Get notice board data
+ * @apiGroup NoticeBoard
+
+ * @apiDescription Mock API for getting alerts and critical informations results for notice board.
+ *
+ * @apiParam {String} username Username of current user.
+ *
+ * @apiSuccess (Success) {String} username allgood
+ * @apiSuccessExample Success response
+ *		HTTP/1.1 200 OK
+ *		[
+ *			// alerts and critical informations array
+ *		]
+ *
+ */
 router.get('/', (req, res) => {
-	let userName = req.body.username
-	let now = moment(new Date())
-	let sixMonthsBefore = now.subtract(6, 'months');
+	let userName = req.query.username
 	let cloneNotices = []
-	let alerts = []
-	let criticalInformations = []
 	let status = null
 	let result = {}
 
 	// Step 1 check userName exits or not, if not, response error with http 403, otherwise, go ahead
-	if(!userName) {
+	if (!userName) {
 		status = 403
 		result = { error: 'Sorry we need your username to get notice data' }
 
 		res.status(status)
 		res.send(result)
 
-		return false;
+		return false
 	}
 
 	// Step 2 Get all alerts and critical information by current userName and combine into one array
-	cloneNotices = getNoticeDataByUserName(jsonAlerts, criticalInformations, userName)
+	cloneNotices = getNoticeDataByUserName(jsonAlerts, jsonCriticalInformations, userName)
 
-	// Step 3 Filter by system_distribution_time, and only keep recently 6 months data
+	// // Step 3 Filter by system_distribution_time, and only keep recently 6 months data
 	cloneNotices = filterOutNoticeEalierThanSixMonthsAgo(cloneNotices)
 
-	// Step 4 Sort by system_distribution_time column in DESC
-	cloneNotices.sort(sortNoticesBySystemDistributionTimeDESC)	
+	// // Step 4 Sort by system_distribution_time column in DESC
+	cloneNotices.sort(sortNoticesBySystemDistributionTimeDESC)
 
 	// Step 5 response the data result with http 200
-	status = 200;
+	status = 200
 	result = cloneNotices
 
 	res.status(status)
--- server-simulator/API/notice-board/notice-board.spec.js
+++ server-simulator/API/notice-board/notice-board.spec.js
@@ -1,36 +1,36 @@
-import React from 'react'
-import { shallow } from 'enzyme'
-import NoticeBoardMockAPI from './index'
+// import React from 'react'
+// import { shallow } from 'enzyme'
+// import NoticeBoardMockAPI from './index'
 
-let responseStatusEvent = sinon.spy()
-let responseSendEvent = sinon.spy()
-let response = {
-	status: responseStatusEvent,
-	send: responseSendEvent
-}
+// let responseStatusEvent = sinon.spy()
+// let responseSendEvent = sinon.spy()
+// let response = {
+// 	status: responseStatusEvent,
+// 	send: responseSendEvent
+// }
 
-describe('NoticeBoard MockAPI', () => {
-	it('will response in 403 http code when could not get username from request', () => {
-		
-	})
+// describe('NoticeBoard MockAPI', () => {
+// 	it('will response in 403 http code when could not get username from request', () => {
 
-	it('will response in 200 http code when could get username from request', () => {
-		
-	})
+// 	})
 
-	it('will response an array when could get username from request', () => {
-		
-	})
+// 	it('will response in 200 http code when could get username from request', () => {
 
-	it('will not response any notices earlier than six months ago', () => {
-		
-	})
+// 	})
 
-	it('will response an array sort by system_distribution_time column in DESC', () => {
-		
-	})
+// 	it('will response an array when could get username from request', () => {
 
-	it('will only response an array with alert_name within "Alert" and "Critical" ', () => {
-		
-	})
-})
+// 	})
+
+// 	it('will not response any notices earlier than six months ago', () => {
+
+// 	})
+
+// 	it('will response an array sort by system_distribution_time column in DESC', () => {
+
+// 	})
+
+// 	it('will only response an array with alert_name within "Alert" and "Critical" ', () => {
+
+// 	})
+// })
