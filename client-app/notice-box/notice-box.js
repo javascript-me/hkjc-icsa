c4eae5d2f81c78229c65da0888bf2b3a7d9ff2d7
Steven_Weng <Steven_Weng@epam.com>
2016-11-30 17:28:11 +0800

    modify the date format to 'DD MMM YYYY hh:mm:ss'

--- client-app/notice-box/notice-box.js
+++ client-app/notice-box/notice-box.js
@@ -43,7 +43,7 @@ export default class NoticeBox extends React.Component {
 	}
 
 	formatDistibutionTime (dateStr) {
-		return Moment(dateStr).format('hh:mm:ss')
+		return Moment(dateStr).format('DD MMM YYYY hh:mm:ss')
 	}
 
 	textEllipsisWhenOverflow (text) {
