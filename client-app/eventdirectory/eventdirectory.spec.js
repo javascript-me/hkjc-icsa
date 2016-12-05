4d8e2e7117912f148ea35854bf2eeb15c133158b
Bing Hu <Bing_Hu@epam.com>
2016-12-05 14:02:05 +0800

    fix lint

--- client-app/eventdirectory/eventdirectory.spec.js
+++ client-app/eventdirectory/eventdirectory.spec.js
@@ -9,14 +9,14 @@ describe('<EventDirectory />', () => {
 		expect(wrapper.find('div.row-eventdirectory')).to.have.length(1)
 
 		const instance = wrapper.instance()
-		
+
 		let selStr = ''
 
 		selStr = 'div.row-eventdirectory .nav-tabs #li-football'
 		expect(wrapper.find(selStr)).to.have.length(1)
 		wrapper.find(selStr).simulate('click')
-		expect(0 === instance.state.curTabIndex).to.be.true
+		expect(instance.state.curTabIndex === 0).to.be.true
 		wrapper.find(selStr).simulate('click')
-		expect(0 === instance.state.curTabIndex).to.be.false
+		expect(instance.state.curTabIndex === 0).to.be.false
 	})
 })
