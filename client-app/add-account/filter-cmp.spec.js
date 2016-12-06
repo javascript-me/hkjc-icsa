0af5ec0286f3184b5b616a601cb62f29f16c2408
Ethan_Liang <Ethan_Liang>
2016-12-06 14:26:15 +0800

    unit test

---
+++ client-app/add-account/filter-cmp.spec.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import _ from 'lodash'
+
+import Filter from './filter-cmp.js'
+import Container from './index.js'
+let filterData = require('../../server-simulator/API/json/baseUserProfile.json')
+
+describe('<Filter />', () => {
+	it('renders filter pannel', () => {
+		const filterCmp = shallow(<Filter />)
+		expect(filterCmp.find('div.body')).to.have.length(1)
+	})
+
+	it('renders header', () => {
+		let HeaderCmp = Filter.__get__('TableHeader')
+		const headerData = Container.__get__('header')
+		const sortInfo = { index: null, sortType: null }
+		const header = shallow(<HeaderCmp header={headerData} sortInfo={sortInfo} />)
+		expect(header.find('th')).to.have.length(headerData.length + 1)
+	})
+
+	it('renders body', () => {
+		let BodyCmp = Filter.__get__('TableRow')
+		const data = filterData
+		const headerData = Container.__get__('header')
+		const fields = _.map(headerData, item => item.field)
+		const body = shallow(<BodyCmp data={data} fields={fields} />)
+		expect(body.find('tr')).to.have.length(data.length)
+	})
+})
