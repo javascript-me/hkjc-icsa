9d7adcd7079ea250884b4839c98769a4cf436106
Bing Hu <Bing_Hu@epam.com>
2016-12-22 17:08:04 +0800

    mock list api

---
+++ server-simulator/API/actions/actions-util-spec.js
@@ -0,0 +1,24 @@
+const allActions = require('../json/actions.json')
+import ActionsUtil from './actions-util'
+
+it('listFilter()', () => {
+	var filteredResult
+
+	filteredResult = ActionsUtil.listFilter(allActions, {
+		userID: 'JC10001',
+		keyWord: ''
+	})
+	expect(filteredResult.length > 0).to.be.true
+
+	filteredResult = ActionsUtil.listFilter(allActions, {
+		userID: 'JC10001',
+		keyWord: 'complete'
+	})
+	expect(filteredResult.length > 0).to.be.true
+
+	filteredResult = ActionsUtil.listFilter(allActions, {
+		userID: 'JC10001',
+		keyWord: 'xxxxyyyyyzzzzz'
+	})
+	expect(filteredResult.length === 0).to.be.true
+})
