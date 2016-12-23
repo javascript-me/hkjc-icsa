f1442daf0c84074b68901aa3b9654b6c7479a9bf
agileabc <agileabc@sina.com>
2016-12-23 18:16:00 +0800

    [HKJCICSA-672] Fix conflict with QA.

--- client-app/communication/notifications/notifications.js
+++ client-app/communication/notifications/notifications.js
@@ -378,10 +378,10 @@ export default React.createClass({
 					<span className='header-title'>{'Task'}</span>
 				</div>
 			</div>
-			<div className='messages-container'>
-				<TabBar onChangeTab={this.changeTab} tabData={this.state.tabData} displayPosition={this.state.taskPanelPosition} />
-				<NoticeBox notices={this.state.noticeBoxData.allNotices} visible={this.state.allNoticesVisible} displayPosition={this.state.taskPanelPosition} onOpenDetail={this.openDetail} onDoAcknowledgement={this.doAcknowledgement} />
-				<NoticeBox notices={this.state.noticeBoxData.unreadNotices} visible={this.state.unreadNoticesVisible} displayPosition={this.state.taskPanelPosition} onOpenDetail={this.openDetail} onDoAcknowledgement={this.doAcknowledgement} />
+			<div className='messages-container colour-container'>
+				<TabBar onChangeTab={this.changeTab} tabData={this.state.tabData} displayPosition={this.state.displaySettingsForTaskPanel} />
+				<NoticeBox notices={this.state.noticeBoxData.allNotices} visible={this.state.allNoticesVisible} displayPosition={this.state.displaySettingsForTaskPanel} onOpenDetail={this.openDetail} onDoAcknowledgement={this.doAcknowledgement} />
+				<NoticeBox notices={this.state.noticeBoxData.unreadNotices} visible={this.state.unreadNoticesVisible} displayPosition={this.state.displaySettingsForTaskPanel} onOpenDetail={this.openDetail} onDoAcknowledgement={this.doAcknowledgement} />
 			</div>
 		</div>
 
