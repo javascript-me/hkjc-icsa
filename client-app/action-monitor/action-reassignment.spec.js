dc5d244516f58d39400719c4ae9fdc39c03b1654
Bing Hu <Bing_Hu@epam.com>
2016-12-27 18:20:01 +0800

    fix test

--- client-app/action-monitor/action-reassignment.spec.js
+++ client-app/action-monitor/action-reassignment.spec.js
@@ -3,9 +3,25 @@ import { shallow } from 'enzyme'
 
 import ActionReassignment from './action-reassignment'
 
+const reassignTask = {}
+
+const usersData = [
+	{
+		displayName: 'Mike Wang',
+		userID: 'JC10003',
+		position: 'Trader'
+	}
+]
+const rolesData = [
+	{
+		roleId: '001',
+		roleName: 'Trading Solutions Analyst'
+	}
+]
+
 describe('<ActionReassignment />', () => {
 	it('test render', () => {
-		const wrapper = shallow(<ActionReassignment />)
+		const wrapper = shallow(<ActionReassignment task={reassignTask} users={usersData} roles={rolesData} />)
 		expect(wrapper.find('div.action-reassignment')).to.have.length(1)
 	})
 })
