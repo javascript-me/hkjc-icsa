9e7db5dc0ff184482061073e296c9899470b6d69
Bing Hu <Bing_Hu@epam.com>
2017-01-04 09:55:16 +0800

    adjust action monitor

--- client-app/notice-box/noticelist.js
+++ client-app/notice-box/noticelist.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ClassNames from 'classnames'
 import TaskDetail from '../task-detail'
-import ActionReassignment from '../action-monitor/action-reassignment'
+import {ActionReassignment} from '../action-monitor'
 
 export default class NoticeList extends React.Component {
 
